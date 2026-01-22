591184586ba8df93618953ead1fffbea03692d8c
Doss MK <falcon7.learnings@gmail.com>
2026-01-22 19:09:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const swaggerOptions = {
     info: {
       title: 'Developers API',
       version: '1.0.0',
-      description: 'A simple API to manage dev-updatennnnnnnnnnnnnnn'
+      description: 'A simple API to manage dev-updatennnnnnnnnnnnnnn1'
     },
     components: {
       securitySchemes: {

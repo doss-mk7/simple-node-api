2ca6ce8bf2c84379d072de20ede9a5864d5cad0f
Doss MK <falcon7.learnings@gmail.com>
2026-01-27 12:52:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const swaggerOptions = {
     info: {
       title: 'Developers API',
       version: '1.0.0',
-      description: 'A simple API to manage dev-ddd-updated vetri -- success --vignesh'
+      description: 'A simple API to manage dev-ddd-updated vetri -- success --vignesh -- testing'
     },
     components: {
       securitySchemes: {
